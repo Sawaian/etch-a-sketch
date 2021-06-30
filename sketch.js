fa722d96c23321eb3c0c0dda19c2607b2f2eac25
Keith <Keith.macario@gmail.com>
2021-06-29 21:14:25 -0700

    The link to the stylesheet was not correct. I referenced RPS-ui to see what I did wrong. Now the stylesheet is effecting the divs created. They've been given margin, so I can more clearly see them apart. For now I'll keep them this way until I can figure out how to set them in a row.

--- sketch.js
+++ sketch.js
@@ -2,13 +2,32 @@
 
 
 
-function cellCreation(){
-for(i = 0; i < 16; ++i){
+let cellCreation = function(){
+for(i = 0; i < 15; ++i){
     let pixl = document.querySelector('#cell');
-    let pixel = document.createElement('p');
+    let pixel = document.createElement('div');
+    pixel.classList.add("pixel");
     pixel.setAttribute('id', "pixel");
     pixl.appendChild(pixel);
-    document.getElementById("pixel").textContent = "Test";
+    document.getElementById("pixel").textContent = "Test"; //It should show test 16 times
+    // but it isn't. I'm missing something here and I'm not sure what.
+    // When the for loop executes, it brings in all of this content. 
+    //cell is selection, we create element p.
+    //Is it possible that it's overwriting itself?
+    console.log("Is it working?");
+    for(i = 0; i < 15; ++i){
+        let pixl = document.querySelector('#cell');
+        let pixel = document.createElement('div');
+        pixel.classList.add("pixel");
+        pixel.setAttribute('id', "pixel");
+        pixl.appendChild(pixel);
+        document.getElementById("pixel").textContent = "Test"; //It should show test 16 times
+        // but it isn't. I'm missing something here and I'm not sure what.
+        // When the for loop executes, it brings in all of this content. 
+        //cell is selection, we create element p.
+        //Is it possible that it's overwriting itself?
+        console.log("Is it working?");
+    }
 }
 
 }
