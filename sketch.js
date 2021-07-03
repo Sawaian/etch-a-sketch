8503eb13362e752a5e3bdbb9070fcbaba145d854
Keith <Keith.macario@gmail.com>
2021-07-03 03:25:35 -0700

    Got the reset button to work one white square on the grid. It is a similar issue to how the whole grid is wasn't changing save for one pixel.

--- sketch.js
+++ sketch.js
@@ -1,5 +1,14 @@
 
 
+let btn = document.querySelector('#reset');
+    btn.addEventListener('click', ()=> {
+    pixel.style.background = 'white';
+  });
+
+//function reset(){
+  //cellCreation();
+
+  //}
 
 
 let cellCreation = function(){
@@ -9,28 +18,12 @@ for(i = 0; i < 256; ++i){
     pixel.classList.add("pixel");
     pixel.setAttribute('id', "pixel");
     pixl.appendChild(pixel);
-    document.getElementById("pixel").textContent = "Test"; //It should show test 16 times
-    // but it isn't. I'm missing something here and I'm not sure what.
-    // When the for loop executes, it brings in all of this content. 
-    //cell is selection, we create element p.
-    //Is it possible that it's overwriting itself?
+    document.getElementById("pixel").textContent = "Test"; 
 
     // changes color of background.
-
-
-
-   // let divColor = document.getElementById( 'pixel' );
-   // divColor.onmouseover = function() {
-   //   this.style.backgroundColor = 'red';
-     // let pixelColor = this.getElementsByTagName( 'pixelColor' );
-      //pixelColor.style.backgroundColor = 'green';
-   // }
-
     pixel.addEventListener('mouseover', function(){
       pixel.style.background = 'pink';
     });
-
-  
 
 }
 }
