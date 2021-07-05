a1e6ba9ff1262c58ab517e2357ee0f6f54a7b8db
Keith <Keith.macario@gmail.com>
2021-07-05 13:21:02 -0700

    Another programmer gave me the answer. I'm still disappointed that he did. but I intend to analyze the solution. pixel.style.height = 'x/y' wherein x is the grid and y is the grid size.

--- sketch.js
+++ sketch.js
@@ -17,14 +17,14 @@
 
       let pixel = document.createElement('div');
       pixel.classList.add("pixel");
+      pixel.style.height=(650/gridSize)+"px"
       container.appendChild(pixel);                                                
 
       // changes color of background.
       pixel.addEventListener('mouseover', function(){
         pixel.style.background = 'pink';
       });
-
-
+      
       let btn = document.querySelector('#reset');
       btn.addEventListener('click', ()=> {
       pixel.style.background = 'white';
@@ -35,5 +35,5 @@
 
 
 container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-//container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 window.addEventListener(onload, divGrid());
