c15fe7ba471a7e91078a1895495086d36b52b92e
Keith <Keith.macario@gmail.com>
2021-07-06 22:22:58 -0700

    Rainbow functionality success! I targeted the class of pixel. It was a period and not a hashtag. Creating a switch case to use.

--- sketch.js
+++ sketch.js
@@ -2,43 +2,55 @@
    const gridSize = prompt();
    let grid = gridSize * gridSize;
    let container = document.querySelector('#container');
-
-   
-
- let divGrid = function() {  
-  for(i = 0; i < grid; ++i){
-      cellCreation();
-     }
-    }
-
+   let etchColor = `pink`;
+  
   
 
   let cellCreation = function(){
 
-      let pixel = document.createElement('div');
-      pixel.classList.add("pixel");
-      //pixel.style.height=(650/gridSize)+"px"
-      container.appendChild(pixel);                                                
+    for(i = 0; i < grid; ++i){  
+    
+      let pixelContainer = document.createElement('div');
+      pixelContainer.classList.add("pixel");
+      container.appendChild(pixelContainer);
 
-      // changes color of background.
-      pixel.addEventListener('mouseover', function(){
-        pixel.style.background = 'pink';
-      });
-      
-      let btn = document.querySelector('#reset');
-      btn.addEventListener('click', ()=> {
-      pixel.style.background = 'white';
-      });
+      let pixel = document.querySelectorAll('.pixel');
+      pixel.forEach(pixel => pixel.addEventListener('mouseover', colorChange));
 
+     // let btn = document.querySelector('#reset');
+      //btn.addEventListener('click', ()=> {
+     // pixel.style.background = 'white';
+    //  });
     }
 
+      // changes color of background.
+    //  pixel.addEventListener('mouseover', function(event){
+      //  pixel.style.background = 'pink';
+      //});
 
-   // let colorChange = () =>{
-    //  Math.floor(Math.random() * 16777215);
-   //   document.pixel.style.backgroundColor = colorChange;
-  //  }
+  //  let rainbowButton = document.querySelector('#rainbow');
+    //rainbowButton.addEventListener('click', () => {
+      //document.this.style.background = colorChange();
+      //console.log("Rainbow!")
+   // });
 
+      function colorChange(){
+        switch (etchColor){
+        case 'pink':
+          this.style.backgroundColor = 'pink';
+        console.log('mouse over works');
 
+        case 'rainbow':
+          this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } 
+      }
+    }
+        //let colorChange = () =>{
+        //Math.floor(Math.random() * 16777215);
+        //document.this.style.backgroundColor = colorChange;
+        //}
+
+    
 container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-window.addEventListener(onload, divGrid());
+window.addEventListener(onload, cellCreation());
